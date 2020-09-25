1ec56f89bfd5d9d8058603834b0b90e0ba44b538
Nicholas Devenish <ndevenish@gmail.com>
2020-09-25 12:49:41 +0100

    Reformat everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const path = require("path");
 
 const rules = [
-  {
-    test: /\.js$/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
-      options: {
-        presets: ["@babel/preset-env", "@babel/preset-react"],
-      },
+    {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+        },
     },
-  },
-  // Wanted by
-  // https://dev.to/siradji/react-without-create-react-app-start-building-your-react-project-like-a-professional-1hih
-  // for css processing stuff. Leaving out for minimalism here.
-  // {
-  //   test: /\.css$/,
-  //   exclude: /node_modules/,
-  //   use: ["style-loader", "css-loader"],
-  // }
+    // Wanted by
+    // https://dev.to/siradji/react-without-create-react-app-start-building-your-react-project-like-a-professional-1hih
+    // for css processing stuff. Leaving out for minimalism here.
+    // {
+    //   test: /\.css$/,
+    //   exclude: /node_modules/,
+    //   use: ["style-loader", "css-loader"],
+    // }
 ];
 
 module.exports = {
-  mode: "development",
-  entry: "./src/js/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "static"),
-  },
-  module: { rules },
-  devServer: {
-    contentBase: path.join(__dirname, "static"),
-  },
+    mode: "development",
+    entry: "./src/js/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "static"),
+    },
+    module: { rules },
+    devServer: {
+        contentBase: path.join(__dirname, "static"),
+    },
 };
